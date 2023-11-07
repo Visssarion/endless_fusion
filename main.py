f6680d4eff174693ee9b0ad42fe24821983d321c
Vissa <46628714+Visssarion@users.noreply.github.com>
2023-11-07 22:32:23 +0300

    Added fullscreen [TO BE CHANGED] Note: Current version has fullscreen exclusive and borderless window. Problem with borderless is that it messes with OS stuff. Probably should only leave exclusive. Note 2: needs to be moved from main to separate script.

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import pygame
 from sys import exit
+from os import environ
 import scripts.pieces as pieces
 import scripts.board as board
 import scripts.config_manager as config_manager
 
 pygame.init()
 
+screen_x, screen_y = pygame.display.set_mode().get_size()
+
 config_manager.load()
 
-screen_scale = (4, 4)
-final_screen = pygame.display.set_mode((160 * screen_scale[0], 90 * screen_scale[1]))
+if config_manager.config["screen"]["fullscreen"]:
+    if config_manager.config["screen"]["exclusive"]:
+        final_screen = pygame.display.set_mode((screen_x, screen_y), pygame.FULLSCREEN)
+    else:
+        environ['SDL_VIDEO_CENTERED'] = '1'
+        final_screen = pygame.display.set_mode((screen_x, screen_y), pygame.NOFRAME)
+        print(final_screen.get_size())
+else:
+    environ['SDL_VIDEO_CENTERED'] = '0'
+    screen_scale = (config_manager.config["screen"]["scale"], config_manager.config["screen"]["scale"])
+    final_screen = pygame.display.set_mode((160 * screen_scale[0], 90 * screen_scale[1]), 0)
 
 screen = pygame.Surface((160, 90))
 pygame.display.set_caption("Endless Fusion")
@@ -26,6 +38,7 @@ game_board = board.Board()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            environ['SDL_VIDEO_CENTERED'] = '0'
             pygame.quit()
             exit()
 
@@ -37,6 +50,7 @@ while True:
     game_board.append_piece(pieces.Piece(pieces.PieceType.PENGUIN, 2, 1))
 
     game_board.render(screen)
-    final_screen.blit(pygame.transform.scale_by(screen, screen_scale), (0, 0))
+    # final_screen.blit(pygame.transform.scale_by(screen, screen_scale), (0, 0))
+    final_screen.blit(pygame.transform.scale(screen, final_screen.get_size()), (0, 0))
     pygame.display.update()
     clock.tick(FPS)
--- scripts/config_manager.py
+++ scripts/config_manager.py
@@ -3,14 +3,15 @@ import json
 config = {
     "screen":
         {
-            "full-screen": False,
-            "x_scale": 4,
-            "y_scale": 4
+            "fullscreen": False,
+            "exclusive": False,
+            "scale": 4
         }
 }
 
 
 def load():
+    global config
     try:
         with open("config.json", "r", encoding='utf-8') as file:
             config = json.loads(file.read())
@@ -19,5 +20,6 @@ def load():
 
 
 def save():
+    global config
     with open("config.json", "w", encoding='utf-8') as file:
         file.write(json.dumps(config))
