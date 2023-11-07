9b75d11e01fa3a12795ff1d124c5ba5b34073937
Vissa <46628714+Visssarion@users.noreply.github.com>
2023-11-07 18:26:35 +0300

    Added FPS clock

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((640, 360))
 pygame.display.set_caption("Endless Fusion")
+clock = pygame.time.Clock()
+
+FPS = 60
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-        pygame.display.update()
+    pygame.display.update()
+    clock.tick(FPS)
+
