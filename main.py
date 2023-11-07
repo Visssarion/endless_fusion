f31ad4b5b540e0b8a23828941856537e7faf33a2
Vissa <46628714+Visssarion@users.noreply.github.com>
2023-11-07 18:42:25 +0300

    Added background and game icon

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pygame
 from sys import exit
 
-
 pygame.init()
-screen = pygame.display.set_mode((640, 360))
+screen = pygame.display.set_mode((160, 90))
 pygame.display.set_caption("Endless Fusion")
 clock = pygame.time.Clock()
 
 FPS = 60
+
+background = pygame.image.load("sprites/playing board.png")
+game_icon = pygame.image.load('sprites/icon.png')
+pygame.display.set_icon(game_icon)
 
 while True:
     for event in pygame.event.get():
@@ -15,6 +18,7 @@ while True:
             pygame.quit()
             exit()
 
+    screen.blit(background, (0, 0))
+
     pygame.display.update()
     clock.tick(FPS)
-
