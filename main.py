7d72d184af75a0bcd4b3b368b9c22621937c0b26
Vissa <46628714+Visssarion@users.noreply.github.com>
2023-11-09 12:23:42 +0300

    Updated main.py for testing

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import scripts.pieces as pieces
 import scripts.board as board
 import scripts.config_manager as config_manager
 import scripts.fullscreen_manager as fullscreen_manager
+import scripts.mouse as mouse_util
+import scripts.pieces_queue as pieces_queue
 
+print("Loading the game...\nPlease wait. :)")
 pygame.init()
 
 config_manager.load()
@@ -16,7 +19,6 @@ if config_manager.config["screen"]["fullscreen"]:
         final_screen = fullscreen_manager.maximized_window()
 else:
     final_screen = fullscreen_manager.windowed()
-
 
 screen = pygame.Surface((160, 90))
 pygame.display.set_caption("Endless Fusion")
@@ -30,21 +32,26 @@ pygame.display.set_icon(game_icon)
 
 game_board = board.Board()
 
+p_queue = pieces_queue.PiecesQueue()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = mouse_util.screen_to_game_pos(pygame.mouse.get_pos(), final_screen.get_size(), screen.get_size())
+            grid_pos = mouse_util.game_to_board_pos(pos, (16, 16), (5, 5))
+            if mouse_util.check_if_in_bound(grid_pos, (4, 4)):
+                if not game_board.piece_exists_at_pos(grid_pos):
+                    game_board.append_piece(pieces.Piece(p_queue.pop(), grid_pos[0], grid_pos[1]))
 
     screen.blit(background, (0, 0))
 
-    game_board.append_piece(pieces.Piece(pieces.PieceType.GOAT, 2, 3))
-    game_board.append_piece(pieces.Piece(pieces.PieceType.FROG, 1, 2))
-    game_board.append_piece(pieces.Piece(pieces.PieceType.FROG, 1, 1))
-    game_board.append_piece(pieces.Piece(pieces.PieceType.PENGUIN, 2, 1))
-
     game_board.render(screen)
-    # final_screen.blit(pygame.transform.scale_by(screen, screen_scale), (0, 0))
+
+    p_queue.render(screen, (90, 3), 0.5, 1, 1)
+
     final_screen.blit(pygame.transform.scale(screen, final_screen.get_size()), (0, 0))
     pygame.display.update()
     clock.tick(FPS)
