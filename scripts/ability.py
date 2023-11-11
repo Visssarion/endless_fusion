0d03534d0053c074a73ee83e68cc60e1027ab1b0
Vissa <46628714+Visssarion@users.noreply.github.com>
2023-11-11 20:17:21 +0300

    Added Ability class

---
+++ scripts/ability.py
@@ -0,0 +1,19 @@
+class Ability:
+    energy: int
+    required_energy: int
+
+    def __init__(self, required_energy: int):
+        self.required_energy = required_energy
+        self.energy = 0
+
+    def add_energy(self, energy: int):
+        self.energy += energy
+
+    def get_coefficient(self):
+        return self.energy / self.required_energy
+
+    def can_be_activated(self):
+        return self.energy > self.required_energy
+
+    def reset(self):
+        self.energy = 0
