01b351e0da811838799363a4765b7ad8ae45dc5a
Vissa <46628714+Visssarion@users.noreply.github.com>
2023-11-12 10:28:40 +0300

    Added game over scaling

--- scripts/game_over.py
+++ scripts/game_over.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.math import lerp
 
 from scripts.singleton import Singleton
 from scripts.time import Time
@@ -7,15 +8,23 @@ from scripts.time import Time
 class GameOverManager(metaclass=Singleton):
     is_game_over: bool = False
     coefficient: float = 0.0
-    time: float = 1.0
+    time: float = 2.0
     game_over_screen: pygame.Surface = pygame.image.load("sprites/game over.png")
 
     def render(self, screen: pygame.Surface):
         if self.is_game_over:
             delta_time = Time.delta_time
-            transparent_vignette: pygame.Surface = self.game_over_screen.copy().convert_alpha()
-            transparent_vignette.set_alpha(int(255 * self.coefficient))
-            screen.blit(transparent_vignette, (0, 0))
+            final_image: pygame.Surface = self.game_over_screen.copy().convert_alpha()
+            final_image.set_alpha(int(255 * self.coefficient))
+
+            final_image = pygame.transform.scale_by(final_image, lerp(4, 1, self.coefficient))
+
+            rect = final_image.get_rect()
+            setattr(rect, "center", (80, 45))
+
+
+
+            screen.blit(final_image, rect)
 
             self.coefficient += delta_time / self.time
             self.coefficient = max(min(self.coefficient, 1), 0)
