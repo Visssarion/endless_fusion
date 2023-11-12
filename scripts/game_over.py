2d270b09e1a56c6c10c1703bf2494677544084d9
Vissa <46628714+Visssarion@users.noreply.github.com>
2023-11-12 11:00:39 +0300

    Changed game over duration/animation ease in

--- scripts/game_over.py
+++ scripts/game_over.py
@@ -8,7 +8,7 @@ from scripts.time import Time
 class GameOverManager(metaclass=Singleton):
     is_game_over: bool = False
     coefficient: float = 0.0
-    time: float = 2.0
+    time: float = 1.5
     game_over_screen: pygame.Surface = pygame.image.load("sprites/game over.png")
 
     def render(self, screen: pygame.Surface):
@@ -17,12 +17,10 @@ class GameOverManager(metaclass=Singleton):
             final_image: pygame.Surface = self.game_over_screen.copy().convert_alpha()
             final_image.set_alpha(int(255 * self.coefficient))
 
-            final_image = pygame.transform.scale_by(final_image, lerp(4, 1, self.coefficient))
+            final_image = pygame.transform.scale_by(final_image, lerp(4, 1, self.coefficient**2))
 
             rect = final_image.get_rect()
             setattr(rect, "center", (80, 45))
-
-
 
             screen.blit(final_image, rect)
 
